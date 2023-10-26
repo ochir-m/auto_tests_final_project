5c576af69e6838c1a32f2dc3c891784a5becb0ae
Ochir <mandzhiev.ochir@gmail.com>
2023-10-26 17:37:37 +0400

    add test_promo_offer

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,7 @@
 import time
 
+import pytest
+
 from .pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
@@ -12,3 +14,13 @@ def test_guest_can_add_product_to_basket(browser):
     page.check_the_message_item_added_to_cart()
     page.check_the_message_total_price_in_basket()
 
+@pytest.mark.parametrize('promo_offer', [*range(0, 7), pytest.param(7, marks=pytest.mark.xfail(reason="bugged link")), *range(8, 10)])
+def test_promo_offer(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_button_add_to_basket()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.check_the_message_item_added_to_cart()
+    page.check_the_message_total_price_in_basket()
