15cc59eda268a33097319d2361f397ea7b8136c6
Ochir <mandzhiev.ochir@gmail.com>
2023-10-27 23:51:27 +0400

    move 'go_to_login_page' and 'should_be_login_link' from main_page on base_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,3 @@
-import time
-
 import pytest
 
 from .pages.product_page import ProductPage
@@ -48,3 +46,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_message_dissapeared_after_adding_product()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
